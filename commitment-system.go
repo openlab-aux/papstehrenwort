90bf84379b8cb9f3cdd1ba243b98bd684dadfbed
Profpatsch <mail@profpatsch.de>
2014-08-27 03:42:40 +0200

    Json Config files

--- commitment-system.go
+++ commitment-system.go
@@ -4,24 +4,28 @@ import (
 	"fmt"
 	"time"
 	"net/mail"
-	// "encoding/json"
+	"encoding/json"
 	"os"
 	"os/signal"
+	"log"
+	"io/ioutil"
 )
 
 
 
 type Task struct {
-	title       string // id
-	description string
-	frequency   time.Duration
-	users       []User // already a list (future feature)
+	Title       string // id
+	Description string
+	Frequency   time.Duration
+	Users       []User // already a list (future feature)
 }
 
 type User mail.Address
 
 func main() {
-	// tasks := loadFromJson()
+	file := "tasks.json"
+	tasks := loadFromJson(file)
+	defer saveToJson(file, tasks)
 
 	go uiServer(8080)
 
@@ -33,10 +37,31 @@ func main() {
 	    }
 }
 
-func loadFromJson() []Task {
-	return nil
+func loadFromJson(file string) *[]Task {
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		l := append(make([]Task, 0), Task{"Hello", "This is Olaf.", 100, nil})
+		return &l
+	}
+	var tasks []Task
+	err = json.Unmarshal(b, &tasks)
+	logFatal(err)
+	return &tasks
+}
+
+func saveToJson(file string, tasks *[]Task) {
+	b, err := json.Marshal(tasks)
+	logFatal(err)
+	err = ioutil.WriteFile(file, b, 0644)
+	logFatal(err)
 }
 
 func uiServer(port int) {
 	return
+}
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
