98dd0610a9a3d1c1b2afbd10eadfb125b406387a
Lukas Epple <git@lukasepple.de>
2014-10-29 13:36:01 +0100

    new structure for the handler

--- app/commitment-system.go
+++ app/commitment-system.go
@@ -78,9 +78,7 @@ func (tasks TaskList) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 				*/
 				for taskname, _ := range tasks {
 					if req.Form[taskname] != nil {
-						fmt.Println("The User", req.Form["name"][0], "with email", req.Form["email"][0],
-							"commited theirselves the task", taskname)
-						fmt.Println(tasks[taskname])
+						// TODO: check for existance of the fields
 					}
 				}
 				http.Redirect(w, req, "/", http.StatusFound)
