dcf1b3076b6315c35b031ffaf7ab21ce445a2b7e
Profpatsch <mail@profpatsch.de>
2016-08-06 17:48:54 +0200

    development environment

---
+++ dev-env.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if test ! -e ./papstehrenwort.cabal; then
+    echo "please run from project directory"
+    exit 1
+fi
+
+if (type nix-shell && \
+    type cabal2nix) >/dev/null; then
+
+    cabal2nix . > papstehrenwort.nix \
+        && nix-shell
+
+else
+    exit 1
+fi
