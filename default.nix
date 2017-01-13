b24c6b39afd29b8904af48758388d669b08ee1a4
Profpatsch <mail@profpatsch.de>
2017-01-13 04:02:43 +0100

    make default.nix standalone & add build tools

--- default.nix
+++ default.nix
@@ -1,10 +1,36 @@
 with import <nixpkgs> {};
 let
   hp = haskellPackages.override {
-    overrides = with haskell.lib; self: super: {
-      papstehrenwort = self.callPackage ./papstehrenwort.nix {};
-      papstehrenwortEnv = addBuildDepends self.papstehrenwort [ self.ghcid ];
+    overrides = with haskell.lib; self: super: with self;
+    let
+      libraryHaskellDepends = [
+        aeson base blaze-html blaze-markup cron errors http-api-data
+        http-media lens mime-mail persistent persistent-template protolude
+        servant servant-blaze servant-server time url wai warp
+      ];
+      executableHaskellDepends = [ protolude warp ];
+      testHaskellDepends = [
+        base protolude smallcheck tasty tasty-hunit tasty-smallcheck time
+      ];
+      allHaskellDepends = libraryHaskellDepends ++ executableHaskellDepends
+                         ++ testHaskellDepends;
+
+    in {
+      papstehrenwort =
+        let hoogle = hoogleLocal { packages = allHaskellDepends; };
+        in self.mkDerivation {
+          pname = "papstehrenwort";
+          version = "0.1.0.0";
+          src = ./.;
+          isLibrary = true;
+          isExecutable = true;
+          description = "Commit yourself to a task. Like a pope.";
+          license = stdenv.lib.licenses.agpl3;
+
+          inherit libraryHaskellDepends executableHaskellDepends testHaskellDepends;
+          buildTools = [ ghcid hoogle ];
+      };
     };
   };
 in
-  hp.papstehrenwortEnv
+  hp.papstehrenwort
