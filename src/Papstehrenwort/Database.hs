042207c62698778ded9219ee1ad2c3b510ac6246
Profpatsch <mail@profpatsch.de>
2017-01-13 04:01:47 +0100

    add database types and conversion functions

---
+++ src/Papstehrenwort/Database.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE NoImplicitPrelude, TemplateHaskell, QuasiQuotes, MultiParamTypeClasses, TypeFamilies, GADTs, GeneralizedNewtypeDeriving #-}
+module Papstehrenwort.Database
+  (
+
+  ) where
+
+import Protolude
+import Data.Time.Calendar (Day)
+
+import Database.Persist.TH
+
+import Papstehrenwort.Types
+
+mkPersist sqlSettings [persistLowerCase|
+  DBTask sql=task
+    title Text
+    description Text
+    how Text Maybe
+    url Text Maybe
+    recur Int
+    start Day
+    deriving Show Eq
+  DBSplasher sql=splasher
+    nick Text
+    mail Text
+    deriving Show Eq
+|]
+
+-- we need these for minimal serialization
+-- also because we don’t want persistent to generate our logical
+-- datatypes for us.
+-- meh, boilerplate
+
+taskToDb :: Task -> DBTask
+taskToDb (Task tit desc how url recur start) =
+  DBTask tit desc how (toS . exportURL <$> url) recur start
+
+dbToTask :: DBTask -> Task
+dbToTask (DBTask tit desc how url recur start) =
+  Task tit desc how (importURL . toS <$> url) recur start
+
+splasherToDb :: Splasher -> DBSplasher
+splasherToDb (Splasher nick mail) = DBSplasher nick mail
+
+dbToSplasher :: DBSplasher -> Splasher
+dbToSplasher (DBSplasher nick mail) = Splasher nick mail
--- src/Papstehrenwort/Types.hs
+++ src/Papstehrenwort/Types.hs
@@ -2,25 +2,36 @@
 module Papstehrenwort.Types where
 
 import Protolude
-import Numeric.Natural
 import Data.Time.Calendar (Day)
 import qualified Network.URL as U
-import Data.Aeson (ToJSON(..), FromJSON(..))
+-- import Data.Aeson (ToJSON(..), FromJSON(..))
 
 
+-- | we wrap the @U.URL@ type because it works with Text
+--   and does not provide aeson instances
 newtype URL = URL U.URL deriving (Eq, Show, Generic)
 exportURL :: URL -> Text
 exportURL (URL u) = toS $ U.exportURL u
+importURL :: Text -> Maybe URL
+importURL t = URL <$> U.importURL (toS t)
 
-data Task = Task { tTitle :: Text
-                 , tDescription :: Text
-                 , tHow :: Maybe Text
-                 , tUrl :: Maybe URL
-                 , tRecur :: Natural
-                 , tStart :: Day
-                 } deriving (Show, Eq, Generic)
+-- | A task that needs to be repeatet regularly
+data Task = Task
+  { tTitle :: Text
+  , tDescription :: Text
+  , tHow :: Maybe Text
+  , tUrl :: Maybe URL
+  , tRecur :: Int
+  , tStart :: Day
+  } deriving (Show, Eq, Generic)
+
+-- | A splasher is a papstehrenwort user
+data Splasher = Splasher
+  { sNick :: Text
+  , sMail :: Text
+  } deriving (Show, Eq, Generic)
 
 
-instance ToJSON Task
-instance ToJSON URL where
-  toJSON = toJSON . exportURL
+-- instance ToJSON Task
+-- instance ToJSON URL where
+--   toJSON = toJSON . exportURL
